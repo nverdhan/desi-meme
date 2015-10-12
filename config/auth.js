ed6442fa128f065ab04a1f54cf1e1fc1eaefec1d
Naisheel Verdhan <nverdhan@gmail.com>
2015-10-12 19:50:32 +0530

    facebook auth code changed

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	facebookAuth :{
 		clientID: '1646291515639257',
     	clientSecret: '8a3c63ecc76730f4b414cb5da2fb0c3d',
-    	callbackURL: "http://localhost:3000/auth/facebook/callback"
+    	callbackURL: "http://www.shudhdesimemes.com/auth/facebook/callback"
 	}
 }
